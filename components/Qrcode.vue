650ccc831ff0e224813822c2230058c0db0763e7
Oteri Eyenike <teyenike1@gmail.com>
2022-04-21 11:44:18 +0100

    Add: restaurant qr code menu

---
+++ components/Qrcode.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  <section class="row container">
+    <div class="row__left">
+      <h1>Restaurant QR Code Menu</h1>
+      <p>Scan the QR code to view our menu</p>
+    </div>
+    <qrcode value="https://www.mcdonalds.com/us/en-us/full-menu.html" :options="{
+      color: {
+        dark: '#FFC72C',
+        light: '#DA291C',
+      },
+      width: 300
+    }"></qrcode>
+  </section>
+    <footer>
+      <address>
+        &copy; 2022. Built with Nuxt.js
+      </address>
+    </footer>
+  </div>
+</template>
+
+<script>
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+export default {
+  components: {
+    qrcode: VueQrcode,
+  },
+};
+</script>
